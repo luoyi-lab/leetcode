743af561f527063790f910ccb34203846b9016d4
luoyi-lab <ly20030714@126.com>
2026-08-19 22:53:10 +0800

    黑马07 标识符

---
+++ hm007.cpp
@@ -0,0 +1,14 @@
+/*1.6 标识符 标识符即变量、常量等 给标识符起名时，要遵循以下规则：
+1.标识符不能是关键字
+2.标识符只能由字母、数字、下划线组成
+3.第一个字符必须为字母或下划线
+4.标识符中字母区分大小写
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+   int ab_D = 18;
+   cout << ab_D <<endl;
+    return 0;
+}
